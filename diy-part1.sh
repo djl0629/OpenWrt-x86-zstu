1bb84b1c3f87a06d6e5e0800e8a7e8918058a91e
nANAKo <33384253+djl0629@users.noreply.github.com>
2021-12-04 12:32:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-git clone https://github.com/kiddin9/openwrt-packages package/kiddin9
+git clone https://github.com/kiddin9/openwrt-packages package/openwrt-packages
